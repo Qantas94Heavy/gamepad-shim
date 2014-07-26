2b775029fccfc8f5d5a132ce63d5de47fc4699bb
Qantas94Heavy <qantas94heavy@gmail.com>
2014-07-26 22:58:24 +0800

    temp

---
+++ source/flash/Main.as
@@ -0,0 +1,9 @@
+package {
+  public class Main {
+    public function alert() {
+      return 'Hello world!';
+    }
+  }
+}
+
+///[A-Za-z0-9\-_.~]/
--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,12 @@
-(function () {
+define(function () {
+  // if you're dumb enough to overwrite this, please don't bother being a programmer
+  try {
+    null.e;
+  } catch (e) {
+    var TypeError = e.constructor;
+  }
+  
+  // we only use these to 
   function Gamepad() {
     throw new TypeError('Illegal constructor.');
   }
@@ -11,7 +19,7 @@
     throw new TypeError('Illegal constructor.');
   }
   
-  if (typeof Gamepad !== 'function') {
+  if (typeof navigator.getGamepads !== 'function') {
     window.Gamepad = Gamepad;
     window.GamepadButton = GamepadButton;
     window.GamepadEvent = GamepadEvent;
@@ -21,19 +29,22 @@
   IEFlash.classid = 'clsid:d27cdb6e-ae6d-11cf-96b8-444553540000';
   IEFlash.id = 'gamepad-shim';
   
-  var src = docuemnt.createElement('param');
+  var src = document.createElement('param');
   src.name = 'src';
   src.value = 'gamepad-shim.swf';
   IEFlash.appendChild(src);
   
+  var altText = document.createTextNode("It appears Adobe Flash Player is not enabled. If your browser does not support the HTML5 Gamepad API, you'll need to enable Adobe Flash Player to use your joystick.");
+  
   var otherFlash = document.createElement('object');
   otherFlash.type = 'application/x-shockwave-flash';
   otherFlash.data = 'gamepad-shim.swf';
-  otherFlash.appendChild(document.createTextNode("It appears Adobe Flash Player is not enabled. If your browser does not support the HTML5 Gamepad API, you'll need to enable Adobe Flash Player to use your joystick."));
+  otherFlash.appendChild();
   IEFlash.appendChild(otherFlash);
   
-  document.getElementsByTagName('head')[0].appendChild(IEFlash)
-});
+  document.getElementsByTagName('head')[0].appendChild(IEFlash);
+})();
+
 
 /*
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="gamepad-shim">
