a2202cc526748709012dca52c94a1a46d70a1ea8
Karl Cheng <qantas94heavy@gmail.com>
2014-09-23 16:17:45 +0800

    Reorganise CSS/JS, add swipe events

--- source/js/flash.js
+++ source/js/flash.js
@@ -4,7 +4,7 @@
 define(function () {
   var flash = document.createElement('object');
   flash.type = 'application/x-shockwave-flash';
-  flash.data = 'binary/gamepad-shim.swf?_=' + Date.now();
+  flash.data = '../binary/gamepad-shim.swf?_=' + Date.now();
   flash.id = 'gamepad-shim';
   
   var altText = document.createTextNode('It appears Adobe Flash Player is not enabled. Since your browser does not support the HTML5 Gamepad API, you must enable Adobe Flash Player to use your joystick.');
